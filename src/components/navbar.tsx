d0b6ed78a518befbc4bab852551cc187a773dfd8
liuwllm <liu.wllm@gmail.com>
2024-07-15 19:36:53 -0400

    Add external link indicator for resume

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import Logo from "../../public/logo.svg";
+import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
 
 export default function NavBar() {
     return (
@@ -22,9 +23,12 @@ export default function NavBar() {
                     <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-borealis"></span>
                 </Link>
                 <Link className="group text-lg sm:text-xl font-medium text-slate-800 transition duration-200" href="/William_Liu_Resume.pdf">
-                    <h1 className="text-borealis text-base font-bold">
-                        resume
-                    </h1>
+                    <div className="flex flex-row gap-1 items-center">
+                        <h1 className="text-borealis text-base font-bold">
+                            resume
+                        </h1>
+                        <ArrowOutwardIcon className="text-borealis text-base"/>
+                    </div>
                     <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-borealis"></span>
                 </Link>
             </div>
