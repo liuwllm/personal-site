b5a6f7f09be63ed44fd2492ba1658215939e66aa
liuwllm <liu.wllm@gmail.com>
2024-07-15 20:15:32 -0400

    Add ego stat

--- src/components/index/body.tsx
+++ src/components/index/body.tsx
@@ -4,7 +4,7 @@ export default function Body() {
             <p>I&apos;m currently dual majoring in Computer Science and Business @ <span className="text-borealis">Western University</span>.</p>
             <p>
                 I&apos;m always trying to work on creating a new project or learning something new. 
-                Some of the things I&apos;ve been doing include <span className="text-borealis">language learning</span> (learning Japanese right now!), <span className="text-borealis">graphic design</span>, <span className="text-borealis">drawing</span>, and <span className="text-borealis">gaming</span> (loving Hades II).
+                Some of the things I&apos;ve been doing include <span className="text-borealis">language learning</span> (learning Japanese right now), <span className="text-borealis">graphic design</span>, <span className="text-borealis">drawing</span>, and <span className="text-borealis">gaming</span> (8:40 speedrunning PB for Hades II!).
             </p>
             <div className="flex flex-row text-borealis">
                 
