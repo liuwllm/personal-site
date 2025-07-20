e3e16fddffcc3d5f5fdaa57c99c1e2872dc9858b
liuwllm <liu.wllm@gmail.com>
2025-07-19 23:18:48 -0400

    Add Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,48 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
 import { manrope } from "../../fonts";
 
+const GA_MEASUREMENT_ID = 'G-C3BHQ502EZ'
+
+const handleRouteChange = (url: URL) => {
+  (window as any).gtag('config', GA_MEASUREMENT_ID, {
+    page_path: url,
+  });
+};
+
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
-    <main className={manrope.className}>
-     <Component {...pageProps} />
-    </main>
+    <>
+      {/* Global Site Tag (gtag.js) */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+      <main className={manrope.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
   )
 }
